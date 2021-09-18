b0390e27a8c962dab6686e304708b2a1d2eb14c0
Nameson Gaudel <namesongaudel.ng@gmail.com>
2021-09-18 18:26:00 +0545

    Added tailwindCss and starting frontend part

--- app/src/App.js
+++ app/src/App.js
@@ -1,10 +1,14 @@
-import logo from './logo.svg';
+
 import './App.css';
 import { Link } from 'react-router-dom'
 import { GoogleLogin } from 'react-google-login';
 import axios from 'axios';
 import { useState } from 'react';
 import CheckoutComponent from './checkout.js';
+
+// Component import
+import Navbar from './Navbar';
+
 
 function App() {
   const [user, setUser] = useState();
@@ -32,8 +36,8 @@ function App() {
     console.log(data);
   }
     return (
-    <div className="App">
-      <h2>APP is Running</h2>
+      <div>
+        <Navbar />
           <GoogleLogin
             clientId="187213291535-p0j7bqtg5eoro1ftgrrq0sdlerukqleu.apps.googleusercontent.com"
             buttonText="Login with Google"
